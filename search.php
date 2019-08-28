e9e367568b9beeeaa3547bca3cbb936b82664704
wuchau <wuchau@connect.ust.hk>
2019-08-28 17:18:20 +0800

    search

--- search.php
+++ search.php
@@ -17,10 +17,26 @@ include "includes/header.php";
             <!-- Blog Entries Column -->
             <div class="col-md-8">
                <?php
-                $query = "SELECT * FROM posts";
+                if(isset($_POST['submit'])){
+                $search=$_POST['search'];
+                    
+                    
+                    
+                $query = "SELECT *FROM posts WHERE post_tags LIKE '%$search%'";
+                $search_query = mysqli_query($connection, $query);
                 
-                $select_all_posts_query = mysqli_query($connection,$query);
-            while($row= mysqli_fetch_assoc($select_all_posts_query)){
+                if(!$search_query){
+                    
+                    die("QUERY FAILED" . mysqli_error($connection));
+                }
+                $count = mysqli_num_rows($search_query);
+                
+                if($count ==0){
+                    echo "<h1> NO RESULT</h1>";
+                }else{
+                    
+
+            while($row= mysqli_fetch_assoc($search_query)){
                 $post_title = $row['post_title'];
                 $post_author = $row['post_author'];
                 $post_date = $row['post_date'];
@@ -48,9 +64,14 @@ include "includes/header.php";
 
                 <hr>             
         
-            
-                <?php    }   ?>
+             
+                <?php    }  
 
+                }
+                    
+                    
+                }
+ ?>
 
                 
 
